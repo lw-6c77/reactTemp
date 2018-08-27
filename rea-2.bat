68de7425693bc61711a52740629ef06a3d7f6d27
lukasz <22728282+lw-6c77@users.noreply.github.com>
2018-08-27 10:58:27 +0100

    Update rea-2.bat

--- rea-2.bat
+++ rea-2.bat
@@ -3,6 +3,6 @@ set /p id=remove src content:
 cd %id%
 cd src
 rm -f src/*
-for %%i in (*.*) do if not "%%i"=="index.css" if not "%%i"=="index.js" del /q "%%i"
+rem for %%i in (*.*) do if not "%%i"=="index.css" if not "%%i"=="index.js" del /q "%%i"
 echo done
 timeout 3
